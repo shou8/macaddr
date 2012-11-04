5a8083413b142829c99817d9e3a7b199870e048b
root <root@masa-X58A-UD3R.(none)>
2012-11-04 15:17:10 +0900

    hoge

--- genMACdb.sh
+++ genMACdb.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-grep '(base 16)' oui.txt | gsed -e 's/     (base 16)\t\t/ /g' | gsed -e 's/"/\\"/g' > mac.db
+grep '(base 16)' oui.txt | sed -e 's/     (base 16)\t\t/ /g' | sed -e 's/"/\\"/g' > mac.db
